05672e4a9e6e57ff53dfc5e656854ce7ef33c877
M0rph <dev@arrigo.io>
2022-08-10 18:32:11 +0200

    Hello World!

---
+++ FirstConsoleApp/FirstConsoleApp/FirstConsoleApp.cpp
@@ -0,0 +1,13 @@
+// Copyright Luca Arrigo. All Rights Reserved.
+
+#include <iostream>
+
+/**
+ * Prints Something 
+ * @return Exit status
+*/
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
